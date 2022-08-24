57261d41429bf6f58643dc32a16f28b434d4e8d2
Emmanuel Odhiambo <emmanuel3odhiambo@gmail.com>
2022-08-24 15:33:17 +0300

    domcontentloaded complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+//Set Up an Event Listener for DOMContentLoaded
+//addEventListener` takes a `String` with the name of the event and and a callback function.
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#text').textContent = "This is really cool!";
+  console.log("The DOM has loaded");
+});
+
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+
